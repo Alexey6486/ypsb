bb6e29bb4de78b3abbe55b7e700b946c3492bf7b
Alex6486 <achernov@bellintegrator.com>
2026-07-16 22:51:39 +0300

    sprint6/testing

---
+++ src/services/slices/feed-ws-slice.test.ts
@@ -0,0 +1,106 @@
+import { describe, it, expect } from 'vitest';
+
+import { feedWsSlice, type TFeedWsState } from '@services/slices/feed-ws-slice';
+import { order_card_test } from '@utils/constants';
+
+import type { PayloadAction } from '@reduxjs/toolkit';
+import type { TWsData } from '@utils/types';
+
+const initState: TFeedWsState = {
+  isConnected: false,
+  data: { orders: [], total: 0, totalToday: 0 },
+  error: null,
+  isLoading: false,
+};
+
+describe('feed-ws-slice', () => {
+  it('тестирование начала подключения', () => {
+    // 1. Arrange: Готовим входные данные
+    const action = {
+      type: 'feedWs/connect',
+    };
+
+    // 2. Act: Запускаем редьюсер
+    const result = feedWsSlice.reducer(initState, action);
+
+    // 3. Assert: Проверяем результат
+    expect(result.isLoading).toBeTruthy();
+    expect(result.isConnected).toBeFalsy();
+  });
+
+  it('тестирование разрыва соединения', () => {
+    // 1. Arrange: Готовим входные данные
+    const action = {
+      type: 'feedWs/disconnect',
+    };
+
+    // 2. Act: Запускаем редьюсер
+    const result = feedWsSlice.reducer(initState, action);
+
+    // 3. Assert: Проверяем результат
+    expect(result.isConnected).toBeFalsy();
+    expect(result.isLoading).toBeFalsy();
+  });
+
+  it('тестирование состояния подключенного сокета', () => {
+    // 1. Arrange: Готовим входные данные
+    const action = {
+      type: 'feedWs/onOpen',
+    };
+
+    // 2. Act: Запускаем редьюсер
+    const result = feedWsSlice.reducer(initState, action);
+
+    // 3. Assert: Проверяем результат
+    expect(result.isConnected).toBeTruthy();
+    expect(result.isLoading).toBeFalsy();
+  });
+
+  it('тестирование получения данных по сокету', () => {
+    // 1. Arrange: Готовим входные данные
+    const payload = {
+      orders: [order_card_test],
+      total: 1,
+      totalToday: 1,
+    };
+    const action = {
+      type: 'feedWs/onMessage',
+      payload,
+    } as PayloadAction<TWsData>;
+
+    // 2. Act: Запускаем редьюсер
+    const result = feedWsSlice.reducer(initState, action);
+
+    // 3. Assert: Проверяем результат
+    expect(result.data.orders).toHaveLength(1);
+  });
+
+  it('тестирование получения ошибки по сокету', () => {
+    // 1. Arrange: Готовим входные данные
+    const payload = 'Network error';
+    const action = {
+      type: 'feedWs/onError',
+      payload,
+    } as PayloadAction<string>;
+
+    // 2. Act: Запускаем редьюсер
+    const result = feedWsSlice.reducer(initState, action);
+
+    // 3. Assert: Проверяем результат
+    expect(result.error).toBe(payload);
+  });
+
+  it('тестирование закрытия соединения', () => {
+    // 1. Arrange: Готовим входные данные
+    const action = {
+      type: 'feedWs/onClose',
+    };
+
+    // 2. Act: Запускаем редьюсер
+    const result = feedWsSlice.reducer(initState, action);
+
+    // 3. Assert: Проверяем результат
+    expect(result.isConnected).toBeFalsy();
+    expect(result.isLoading).toBeFalsy();
+  });
+});
---
+++ src/services/slices/profile-ws-slice.test.ts
@@ -0,0 +1,107 @@
+import { describe, it, expect } from 'vitest';
+
+import { profileWsSlice } from '@services/slices/profile-ws-slice';
+import { order_card_test } from '@utils/constants';
+
+import type { PayloadAction } from '@reduxjs/toolkit';
+import type { TFeedWsState } from '@services/slices/feed-ws-slice';
+import type { TWsData } from '@utils/types';
+
+const initState: TFeedWsState = {
+  isConnected: false,
+  data: { orders: [], total: 0, totalToday: 0 },
+  error: null,
+  isLoading: false,
+};
+
+describe('profile-ws-slice', () => {
+  it('тестирование начала подключения', () => {
+    // 1. Arrange: Готовим входные данные
+    const action = {
+      type: 'profileWs/connect',
+    };
+
+    // 2. Act: Запускаем редьюсер
+    const result = profileWsSlice.reducer(initState, action);
+
+    // 3. Assert: Проверяем результат
+    expect(result.isLoading).toBeTruthy();
+    expect(result.isConnected).toBeFalsy();
+  });
+
+  it('тестирование разрыва соединения', () => {
+    // 1. Arrange: Готовим входные данные
+    const action = {
+      type: 'profileWs/disconnect',
+    };
+
+    // 2. Act: Запускаем редьюсер
+    const result = profileWsSlice.reducer(initState, action);
+
+    // 3. Assert: Проверяем результат
+    expect(result.isConnected).toBeFalsy();
+    expect(result.isLoading).toBeFalsy();
+  });
+
+  it('тестирование состояния подключенного сокета', () => {
+    // 1. Arrange: Готовим входные данные
+    const action = {
+      type: 'profileWs/onOpen',
+    };
+
+    // 2. Act: Запускаем редьюсер
+    const result = profileWsSlice.reducer(initState, action);
+
+    // 3. Assert: Проверяем результат
+    expect(result.isConnected).toBeTruthy();
+    expect(result.isLoading).toBeFalsy();
+  });
+
+  it('тестирование получения данных по сокету', () => {
+    // 1. Arrange: Готовим входные данные
+    const payload = {
+      orders: [order_card_test],
+      total: 1,
+      totalToday: 1,
+    };
+    const action = {
+      type: 'profileWs/onMessage',
+      payload,
+    } as PayloadAction<TWsData>;
+
+    // 2. Act: Запускаем редьюсер
+    const result = profileWsSlice.reducer(initState, action);
+
+    // 3. Assert: Проверяем результат
+    expect(result.data.orders).toHaveLength(1);
+  });
+
+  it('тестирование получения ошибки по сокету', () => {
+    // 1. Arrange: Готовим входные данные
+    const payload = 'Network error';
+    const action = {
+      type: 'profileWs/onError',
+      payload,
+    } as PayloadAction<string>;
+
+    // 2. Act: Запускаем редьюсер
+    const result = profileWsSlice.reducer(initState, action);
+
+    // 3. Assert: Проверяем результат
+    expect(result.error).toBe(payload);
+  });
+
+  it('тестирование закрытия соединения', () => {
+    // 1. Arrange: Готовим входные данные
+    const action = {
+      type: 'profileWs/onClose',
+    };
+
+    // 2. Act: Запускаем редьюсер
+    const result = profileWsSlice.reducer(initState, action);
+
+    // 3. Assert: Проверяем результат
+    expect(result.isConnected).toBeFalsy();
+    expect(result.isLoading).toBeFalsy();
+  });
+});
